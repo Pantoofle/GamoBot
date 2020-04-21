9de8e571ecaafd93f6ce3b9f3183efe309b40201
Simon Fernandez <simon.fernandez@ens-lyon.fr>
2020-04-21 18:10:24 +0200

    add relative path to token to allow running the script from another folder

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
-import sys
+import os
 import discord
 import random
 from discord.ext import commands
-from typing import List
 
 from table import *
 
 # Load the bot token
+path = os.path.dirname(os.path.abspath(__file__))
 TOKEN = ""
-with open(".token", "r") as f:
+with open(path+"/.token", "r") as f:
     TOKEN = f.readline()
 
 DELIMITER = '-'
@@ -24,12 +24,12 @@ async def start(ctx, *args):
     txt = " ".join(args).split(DELIMITER)
     if len(txt) < 2:
         await ctx.message.delete()
-        await ctx.send("Format invalide. Utiliser `!start [liste des joueurs] - [nom de la table]`", delete_after = 5)
+        await ctx.send("Format invalide. Utiliser `!start [liste des joueurs] - [nom de la table]`", delete_after=5)
         return
 
     name = txt[1]
     players = ctx.message.mentions
-    await ctx.send("Création de la table " + name, delete_after = 10)
+    await ctx.send("Création de la table " + name, delete_after=10)
 
     guild = ctx.guild
     overwrites = {
@@ -38,8 +38,9 @@ async def start(ctx, *args):
     }
 
     # Create table
-    table_name = "Table "+ name
-    category = discord.utils.find(lambda cat: cat.name == table_name, ctx.guild.categories)
+    table_name = "Table " + name
+    category = discord.utils.find(
+        lambda cat: cat.name == table_name, ctx.guild.categories)
     if not category:
         category = await ctx.guild.create_category(table_name, overwrites=overwrites)
         txt_chan = await ctx.guild.create_text_channel(
@@ -63,9 +64,10 @@ async def start(ctx, *args):
         await table.start()
         await update_tables(ctx.guild)
     else:
-        await ctx.send("Cette table existe déjà. Trouve un autre nom.", delete_after = 5)
+        await ctx.send("Cette table existe déjà. Trouve un autre nom.", delete_after=5)
 
     await ctx.message.delete()
+
 
 @bot.command()
 async def end(ctx):
@@ -80,25 +82,28 @@ async def end(ctx):
         await update_tables(ctx.guild)
     else:
         await ctx.message.delete()
-        await ctx.send("Ce n'est pas une table que je gère. Je vais pas la supprimer.", delete_after = 5)
+        await ctx.send("Ce n'est pas une table que je gère. Je vais pas la supprimer.", delete_after=5)
+
 
 @bot.command()
 async def join(ctx, *args):
     global tables
     name = "table-" + "-".join(args)
-    chan = discord.utils.find(lambda chan: chan.name.lower() == name.lower(), ctx.guild.text_channels)
+    chan = discord.utils.find(
+        lambda chan: chan.name.lower() == name.lower(), ctx.guild.text_channels)
     if chan is not None:
         table = tables[chan]
         if table.open:
             await table.add_player(ctx.author)
         else:
-            await ctx.send("La table est fermée, elle ne prend plus de nouveau joueur", delete_after = 5)
+            await ctx.send("La table est fermée, elle ne prend plus de nouveau joueur", delete_after=5)
     else:
-        await ctx.send("La table " + name + " n'existe pas...", delete_after = 5)
+        await ctx.send("La table " + name + " n'existe pas...", delete_after=5)
 
     await ctx.message.delete()
 
-@bot.command(name = "open")
+
+@bot.command(name="open")
 async def open_table(ctx):
     global tables
     if ctx.channel in tables:
@@ -107,11 +112,12 @@ async def open_table(ctx):
         await table.txt_channel.send("La table est maintenant ouverte au public !")
         await update_tables(ctx.guild)
     else:
-        await ctx.send("Je ne peux faire ça que dans un chan de table de jeu", delete_after = 5)
+        await ctx.send("Je ne peux faire ça que dans un chan de table de jeu", delete_after=5)
 
     await ctx.message.delete()
 
-@bot.command(name = "close")
+
+@bot.command(name="close")
 async def close_table(ctx):
     global tables
     if ctx.channel in tables:
@@ -120,7 +126,7 @@ async def close_table(ctx):
         await table.txt_channel.send("La table est maintenant fermée au public !")
         await update_tables(ctx.guild)
     else:
-        await ctx.send("Je ne peux faire ça que dans un chan de table de jeu", delete_after = 5)
+        await ctx.send("Je ne peux faire ça que dans un chan de table de jeu", delete_after=5)
 
     await ctx.message.delete()
 
@@ -137,10 +143,12 @@ async def update_tables(guild):
             txt += " [FERMÉ]"
 
     if index_msg is None:
-        chan = discord.utils.find(lambda c: c.name == INDEX_CHAN, guild.channels)
+        chan = discord.utils.find(
+            lambda c: c.name == INDEX_CHAN, guild.channels)
         index_msg = await chan.send(txt)
     else:
-        await index_msg.edit(content = txt)
+        await index_msg.edit(content=txt)
+
 
 @bot.command()
 async def roll(ctx, txt):
@@ -155,7 +163,7 @@ async def roll(ctx, txt):
             await ctx.send("Résultat des dés : (**" + "** + **".join([str(v) for v in dices]) + "**) = **" + str(s) + "**")
     except ValueError:
         await ctx.message.delete()
-        await ctx.send("Pour lancer des dés : `!roll <nb de dés>d<nombre de faces>`, par exemple `!roll 3d10`", delete_after = 5)
+        await ctx.send("Pour lancer des dés : `!roll <nb de dés>d<nombre de faces>`, par exemple `!roll 3d10`", delete_after=5)
         return
 
 bot.run(TOKEN)
